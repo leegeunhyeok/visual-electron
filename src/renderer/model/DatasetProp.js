995936669ee609650008f3c10ab1c4d66225a90d
Leegeunhyeok <lghlove0509@naver.com>
2018-06-08 14:00:04 +0900

    Add charts prop data

---
+++ src/renderer/model/DatasetProp.js
@@ -0,0 +1,412 @@
+const prop = {
+  line: [
+    {
+      name: 'label',
+      type: 'string'
+    },
+    {
+      name: 'backgroundColor',
+      type: 'string'
+    },
+    {
+      name: 'borderColor',
+      type: 'string'
+    },
+    {
+      name: 'borderWidth',
+      type: 'number'
+    },
+    {
+      name: 'borderDash',
+      type: 'array'
+    },
+    {
+      name: 'borderDashOffset',
+      type: 'number'
+    },
+    {
+      name: 'borderCapStyle',
+      type: 'string'
+    },
+    {
+      name: 'borderJoinStyle',
+      type: 'string'
+    },
+    {
+      name: 'fill',
+      type: 'boolean'
+    },
+    {
+      name: 'lineTension',
+      type: 'number'
+    },
+    {
+      name: 'pointBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'pointBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'pointBorderWidth',
+      type: 'array'
+    },
+    {
+      name: 'pointRadius',
+      type: 'array'
+    },
+    {
+      name: 'pointStyle',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBorderWidth',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverRadius',
+      type: 'array'
+    },
+    {
+      name: 'showLine',
+      type: 'boolean'
+    },
+    {
+      name: 'spanGaps',
+      type: 'boolean'
+    }
+  ],
+  bar: [
+    {
+      name: 'label',
+      type: 'string'
+    },
+    {
+      name: 'backgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'borderColor',
+      type: 'array'
+    },
+    {
+      name: 'borderWidth',
+      type: 'array'
+    },
+    {
+      name: 'borderSkipped',
+      type: 'string'
+    },
+    {
+      name: 'hoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderWidth',
+      type: 'array'
+    }
+  ],
+  radar: [
+    {
+      name: 'label',
+      type: 'string'
+    },
+    {
+      name: 'backgroundColor',
+      type: 'string'
+    },
+    {
+      name: 'borderColor',
+      type: 'string'
+    },
+    {
+      name: 'borderWidth',
+      type: 'number'
+    },
+    {
+      name: 'borderDash',
+      type: 'array'
+    },
+    {
+      name: 'borderDashOffset',
+      type: 'number'
+    },
+    {
+      name: 'borderCapStyle',
+      type: 'string'
+    },
+    {
+      name: 'borderJoinStyle',
+      type: 'string'
+    },
+    {
+      name: 'fill',
+      type: 'boolean'
+    },
+    {
+      name: 'lineTension',
+      type: 'number'
+    },
+    {
+      name: 'pointBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'pointBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'pointBorderWidth',
+      type: 'array'
+    },
+    {
+      name: 'pointRadius',
+      type: 'array'
+    },
+    {
+      name: 'pointStyle',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBorderWidth',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverRadius',
+      type: 'array'
+    }
+  ],
+  doughnut: [
+    {
+      name: 'backgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'borderColor',
+      type: 'array'
+    },
+    {
+      name: 'borderWidth',
+      type: 'array'
+    },
+    {
+      name: 'hoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderWidth',
+      type: 'array'
+    }
+  ],
+  pie: [
+    {
+      name: 'backgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'borderColor',
+      type: 'array'
+    },
+    {
+      name: 'borderWidth',
+      type: 'array'
+    },
+    {
+      name: 'hoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderWidth',
+      type: 'array'
+    }
+  ],
+  polarArea: [
+    {
+      name: 'backgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'borderColor',
+      type: 'array'
+    },
+    {
+      name: 'borderWidth',
+      type: 'array'
+    },
+    {
+      name: 'hoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'hoverBorderWidth',
+      type: 'array'
+    }
+  ],
+  scatter: [
+    {
+      name: 'label',
+      type: 'string'
+    },
+    {
+      name: 'backgroundColor',
+      type: 'string'
+    },
+    {
+      name: 'borderColor',
+      type: 'string'
+    },
+    {
+      name: 'borderWidth',
+      type: 'number'
+    },
+    {
+      name: 'borderDash',
+      type: 'array'
+    },
+    {
+      name: 'borderDashOffset',
+      type: 'number'
+    },
+    {
+      name: 'borderCapStyle',
+      type: 'string'
+    },
+    {
+      name: 'borderJoinStyle',
+      type: 'string'
+    },
+    {
+      name: 'fill',
+      type: 'boolean'
+    },
+    {
+      name: 'lineTension',
+      type: 'number'
+    },
+    {
+      name: 'pointBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'pointBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'pointBorderWidth',
+      type: 'array'
+    },
+    {
+      name: 'pointRadius',
+      type: 'array'
+    },
+    {
+      name: 'pointStyle',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBackgroundColor',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBorderColor',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverBorderWidth',
+      type: 'array'
+    },
+    {
+      name: 'pointHoverRadius',
+      type: 'array'
+    },
+    {
+      name: 'showLine',
+      type: 'boolean'
+    },
+    {
+      name: 'spanGaps',
+      type: 'boolean'
+    }
+  ],
+  bubble: [
+    {
+      name: 'label',
+      type: 'string'
+    },
+    {
+      name: 'backgroundColor',
+      type: 'string'
+    },
+    {
+      name: 'borderColor',
+      type: 'string'
+    },
+    {
+      name: 'borderWidth',
+      type: 'number'
+    },
+    // {
+    //   name: 'data',
+    //   type: 'array'
+    // }
+    {
+      name: 'hoverBackgroundColor',
+      type: 'string'
+    },
+    {
+      name: 'hoverBorderColor',
+      type: 'string'
+    },
+    {
+      name: 'hoverBorderWidth',
+      type: 'number'
+    },
+    {
+      name: 'hoverRadius',
+      type: 'number'
+    },
+    {
+      name: 'pointStyle',
+      type: 'string'
+    },
+    {
+      name: 'radius',
+      type: 'number'
+    }
+  ]
+}
+
+export default prop
